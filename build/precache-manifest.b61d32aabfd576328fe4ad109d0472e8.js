6a42002c865a9863e0efe3acdb15ee12869af041
Karla <kmcortes7@gmail.com>
2020-10-14 14:14:28 -0400

    Trial 2

---
+++ build/precache-manifest.b61d32aabfd576328fe4ad109d0472e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d0e315ecfcf77617c00073fd54ef28",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "096890b86b1334783a10",
+    "url": "/portfolio-2020/static/css/main.95a3fd40.chunk.css"
+  },
+  {
+    "revision": "dbb5e3a7065702a1ddc7",
+    "url": "/portfolio-2020/static/js/2.996437b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-2020/static/js/2.996437b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096890b86b1334783a10",
+    "url": "/portfolio-2020/static/js/main.68c13e9d.chunk.js"
+  },
+  {
+    "revision": "f94e5f7661e12495014e",
+    "url": "/portfolio-2020/static/js/runtime-main.2e14f2c6.js"
+  },
+  {
+    "revision": "8f18fd852e7ec11b1354893b3a0e23d9",
+    "url": "/portfolio-2020/static/media/RTRondelleTest-Bold.8f18fd85.otf"
+  },
+  {
+    "revision": "a8b7fa31870b05796dda55dce1eae66c",
+    "url": "/portfolio-2020/static/media/RTRondelleTest-Light.a8b7fa31.otf"
+  },
+  {
+    "revision": "540e2c02485cbf4ef3d6d59595da9d0c",
+    "url": "/portfolio-2020/static/media/Simplon-Mono.540e2c02.otf"
+  },
+  {
+    "revision": "2198b2b8256e562d5b2c19fa008b10d1",
+    "url": "/portfolio-2020/static/media/noise.2198b2b8.png"
+  }
+]);
